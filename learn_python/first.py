809b10804496a356cd968f0e13d4ddcffbd7bcf1
jingfeng.shi <shijf@zhonghongcarav.com.cn>
2015-12-28 18:10:28 +0800

    add learn python

--- learn_python/first.py
+++ learn_python/first.py
@@ -10,3 +10,16 @@ print ( x * 8 )
 #load myfile module
 import myfile
 print ( myfile.title )
+
+#index
+S = "Spam"
+print ( "S=" + S[:] )
+print ( 'S[0]=' + S[0] + ' S[-1]=' + S[-1] + ' S[1:3]=' + S[1:3] )
+print ( "S[1:]=" + S[1:] )
+print ( "S[:3]=" + S[:3] )
+print ( "S[:-1]=" + S[:-1] )
+#S[0] = 's'
+#Traceback (most recent call last):
+#  File "./first.py", line 21, in <module>
+#    S[0] = 's'
+#TypeError: 'str' object does not support item assignment
