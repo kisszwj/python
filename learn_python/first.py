22acccd6702a6d9f939232ab83ab26e005254edf
jingfeng.shi <shijf@zhonghongcarav.com.cn>
2015-12-28 14:08:00 +0800

    add python day02 and learn python

---
+++ learn_python/first.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+
+#A first Python script
+import sys  #import sys module for mutile module
+print ( sys.platform )
+print ( 2 ** 100 ) # 2^100
+x = 'Spam!'
+print ( x * 8 )
+
+#load myfile module
+import myfile
+print ( myfile.title )
